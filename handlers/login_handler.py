0e4281052fe3386ece0e3303d4a6f7e3b4f3ab2c
domster704 <domster704@mail.ru>
2024-06-09 20:01:11 +0300

    comment functions in login_handler.py

--- handlers/login_handler.py
+++ handlers/login_handler.py
@@ -2,6 +2,8 @@
 Раздел <Авторизация>
 """
 
+from __future__ import annotations
+
 from aiogram import types, Router, F
 from aiogram.filters import StateFilter
 from aiogram.fsm.context import FSMContext
@@ -21,6 +23,14 @@ loginRouter = Router()
 
 @loginRouter.message(AppState.login)
 async def loginHandlerInit(message: types.Message, state: FSMContext) -> None:
+    """
+    Функция-инициализатор для раздела <Авторизация>. Проверяет авторизован ли пользователь.
+    Если пользователь не авторизован, то проводит процедуру авторизации.
+    Если пользователь авторизован, то переходит в следующий раздел <Информация о пользователь и помощь>
+    :param message:
+    :param state:
+    :return:
+    """
     user_login_info: User = await session.get(User, message.from_user.id)
     if user_login_info is not None and user_login_info.isAuth:
         await goToInfoHandler(message, state)
@@ -33,12 +43,24 @@ async def loginHandlerInit(message: types.Message, state: FSMContext) -> None:
 
 @loginRouter.callback_query(StateFilter(None), F.data == TRY_AGAIN_ACTION)
 async def loginHandlerCallbackInit(callback: types.CallbackQuery, state: FSMContext) -> None:
+    """
+    Функция-инициализатор, которая вызывается при нажатии на inline-кнопку c callback_data={TRY_AGAIN_ACTION}. Кнопка
+    создается в {AuthorizationCheckMiddleware} или в {__checkAuthentication}.
+    :param callback:
+    :param state:
+    """
     await callback.message.answer(ENTER_LOGIN)
     await state.set_state(AuthState.login)
 
 
 @loginRouter.message(AuthState.login)
 async def getLogin(message: types.Message, state: FSMContext) -> None:
+    """
+    Функция, которая вызывается при state=AuthState.login. Записывает введенный логин/почту пользователем и
+    запрашивает ввод пароля
+    :param message:
+    :param state:
+    """
     await state.update_data(login=message.text.lower())
     await message.answer(ENTER_PASSWORD)
     await state.set_state(AuthState.password)
@@ -46,9 +68,29 @@ async def getLogin(message: types.Message, state: FSMContext) -> None:
 
 @loginRouter.message(AuthState.password)
 async def getPassword(message: types.Message, state: FSMContext) -> None:
+    """
+    Функция, которая вызывается при state=AuthState.password. Записывает введенный пароль пользователем и
+    вызывает функцию для проверки введенных данных
+    :param message:
+    :param state:
+    """
     await state.update_data(password=message.text.lower())
     auth: AuthorizationCredentialsChecker = AuthorizationCredentialsChecker(**await state.get_data())
+    await __checkAuthentication(message, state, auth)
 
+
+async def __checkAuthentication(message: types.Message, state: FSMContext,
+                                auth: AuthorizationCredentialsChecker) -> None:
+    """
+    Функция, котора проверяет введенные данные для авторизации.
+    Если логин/почта и пароль верные, то происходит авторизация, путем добавления записи в базу данных.
+    Если логин/почта и пароль не верные, то создается inline-кнопка с callback_data={TRY_AGAIN_ACTION} для
+    повторной авторизации.
+    :param message:
+    :param state:
+    :param auth:
+    :return:
+    """
     try:
         await state.clear()
 
@@ -75,11 +117,21 @@ async def getPassword(message: types.Message, state: FSMContext) -> None:
 
 
 async def goToInfoHandler(message: types.Message, state: FSMContext) -> None:
+    """
+    Функция, которая переходит в раздел <Информация о пользователе и помощь>
+    :param message:
+    :param state:
+    :return:
+    """
     await state.set_state(AppState.info)
     await infoHandlerInit(message, state)
 
 
 class AuthorizationCredentialsChecker(object):
+    """
+    Класс, который проверяет введенные данные для авторизации.
+    """
+
     def __init__(self, login: str, password: str, **kwargs):
         self.__login: str = login
         self.__password: str = password
